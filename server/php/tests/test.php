cc9d5c0ee6be52f14bcfc875e2c17beba45dca10
Joshua Thornton <jshthornton@gmail.com>
2014-05-31 18:48:13 +0100

    Update test.php

--- server/php/tests/test.php
+++ server/php/tests/test.php
@@ -1,35 +1,35 @@
 <?php
 include_once('../src/Output2Web.php');
 
-Output2Web::$dir = __DIR__ . DIRECTORY_SEPARATOR . 'logs';
+Parallelbug::$dir = __DIR__ . DIRECTORY_SEPARATOR . 'logs';
 
-var_dump(Output2Web::dump(array(
+var_dump(Parallelbug::dump(array(
 	'blah'
 ), array(
 	'filename' => 'array'
 )));
 
-var_dump(Output2Web::dump(array(
+var_dump(Parallelbug::dump(array(
 	'foo' => 'blah'
 ), array(
 	'filename' => 'ass_array'
 )));
 
 // Supposed to fail
-var_dump(Output2Web::dump(array(
+var_dump(Parallelbug::dump(array(
 	'foo' => 'blah'
 ), array(
 	'filename' => 'append_fail',
 	'append' => true
 )));
 
-var_dump(Output2Web::dump(array(
+var_dump(Parallelbug::dump(array(
 	'foo' => 'blah'
 ), array(
 	'filename' => 'append'
 )));
 
-var_dump(Output2Web::dump(array(
+var_dump(Parallelbug::dump(array(
 	'foo' => 'blah'
 ), array(
 	'filename' => 'append',
@@ -37,19 +37,19 @@ var_dump(Output2Web::dump(array(
 )));
 
 
-var_dump(Output2Web::dump(true, array(
+var_dump(Parallelbug::dump(true, array(
 	'filename' => 'true'
 )));
 
-var_dump(Output2Web::dump(false, array(
+var_dump(Parallelbug::dump(false, array(
 	'filename' => 'false'
 )));
 
-var_dump(Output2Web::dump((object) array('property' => 'value'), array(
+var_dump(Parallelbug::dump((object) array('property' => 'value'), array(
 	'filename' => 'object'
 )));
 
-var_dump(Output2Web::dump(function() { $balh = 'hello'; }, array(
+var_dump(Parallelbug::dump(function() { $balh = 'hello'; }, array(
 	'filename' => 'function'
 )));
 
